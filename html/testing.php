3bc7062f38efda97f0195ac440c552b9874ed847
qikliang <qik.liang@gmail.com>
2017-04-24 22:50:30 -0700

    init php testing page

---
+++ html/testing.php
@@ -0,0 +1,34 @@
+<?php
+//get username and password to database
+include "../inc/dbinfo.inc";
+
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+if ($conn->connect_error){
+	echo 'Error: unable to connect to server, please try again.';
+	exit();
+}
+
+$query = $conn->query('select * from location');
+
+$data = [];
+while($row = $query->fetch_assoc()){
+	array_push($data, row);
+}
+
+$conn->close();
+?>
+
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Testing PHP</title>
+</head>
+<body>
+	<h1>
+		<?=json_encode($data)?>
+	</h1>
+	//<script charset="utf-8">
+		//var data = JSON.parse(<?=json_encode($data)?>);
+	//</script>
+</body>
+</html>
