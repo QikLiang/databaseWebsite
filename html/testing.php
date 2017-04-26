4bd146eab0239e409482424451b6bf17f30f6b94
qikliang <qik.liang@gmail.com>
2017-04-25 17:36:59 -0700

    try drawImage

--- html/testing.php
+++ html/testing.php
@@ -26,11 +26,11 @@ $conn->close();
 </head>
 <body>
 	<img src="./DBProject_Background.png" id="map" style="display:none;">
-	<canvas id="graphics"></canvas>
+	<canvas id="graphics" style="width:100; height:100;"></canvas>
 	<script charset="utf-8">
 		var canvas = document.getElementById("graphics").getContext("2d");
 		var map = document.getElementById("map");
-		canvas.drawImage(map, 100, 100);
+		canvas.drawImage(map, 0, 0, 100, 100);
 		var body = document.getElementsByTagName("body")[0];
 		var data = JSON.parse('<?=json_encode($data)?>');
 		var len = data.length;
